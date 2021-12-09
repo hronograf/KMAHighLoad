6a0790a970b6a5efabf04ef65683597990928c21
hronograf <vlad.boholii@gmail.com>
2021-12-09 23:01:27 +0200

    added DbWorker tests

--- Common/src/main/java/com/common/book/dto/DeleteBookRequestDto.java
+++ Common/src/main/java/com/common/book/dto/DeleteBookRequestDto.java
@@ -1,10 +1,15 @@
 package com.common.book.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Data
 public class DeleteBookRequestDto implements Serializable {
